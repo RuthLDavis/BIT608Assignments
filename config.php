5ceaa5111b0ebd8fc3618bf0aa29b3be950a7888
RuthLDavis <80608067+RuthLDavis@users.noreply.github.com>
2021-08-20 13:23:04 +1200

    Update config.php
    config details replaced with asterisks for security when making the repository public

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 //MySQL credentails
-define("DBUSER","epiz_28832838");
-define("DBPASSWORD","vH6o7hyHOL72Bf");
-define("DBDATABASE","epiz_28832838_ongaongabandb");
-define("DBHOST","sql210.epizy.com");
+define("DBUSER","***********");
+define("DBPASSWORD","***********");
+define("DBDATABASE","********************");
+define("DBHOST","************");
 ?>
